bb3158325a0da4a6ccf6379a386cff6685c12c6d
Jeff Soloshy <jeffsoloshy@gmail.com>
2019-09-26 16:55:58 -0700

    fix: now wrapping RegisterHook in wrap

--- src/plugin/tasks/struct/snippets/service/src/handler/trigger/register.js
+++ src/plugin/tasks/struct/snippets/service/src/handler/trigger/register.js
@@ -1,4 +1,4 @@
 const { wrap } = require('lambda-cfn-hook');
 const { RegisterHook } = require('../../core/sdu');
 
-module.exports.hook = RegisterHook(process.env.SERVICE_NAME, process.env.SERVICE_URI);
+module.exports.hook = wrap(RegisterHook(process.env.SERVICE_NAME, process.env.SERVICE_URI));
